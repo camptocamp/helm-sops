15d07800e955264b569a68296448bce42ddebbe9
Barahona José Luis (IT-PTR-BDE16) <jose_luis.barahona@sbb.ch>
2024-05-30 15:15:56 +0200

    fix: separate DetectSopsYaml function to unhide marshalling errors

--- utils.go
+++ utils.go
@@ -6,20 +6,22 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func DetectSopsYaml(filename string) (bool, error) {
+func ReadAndUnmarshalYaml(filename string) (map[string]interface{}, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
 	var sf map[string]interface{}
 	err = yaml.Unmarshal(data, &sf)
 	if err != nil {
-		return false, err
+		return nil, err
 	}
 
-	if _, ok := sf["sops"]; ok {
-		return true, nil
-	}
-	return false, nil
+	return sf, nil
+}
+
+func DetectSopsKey(sf map[string]interface{}) bool {
+	_, ok := sf["sops"]
+	return ok
 }
